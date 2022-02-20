d4f9c3200733bcbaaa6cf9e5c28a9f31c8422003
Vedant <wayal.vedant@gmail.com>
2022-02-20 17:09:22 +0530

    Version 2

--- pyntruder.py
+++ pyntruder.py
@@ -1,20 +1,38 @@
-import requests
-from sys import argv
-from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures as cp
 from time import perf_counter
+from sys import argv
+import urllib3
+import requests
 
-url=argv[1]
-threadNo=int(argv[2])
+with open(argv[1],"r") as f:
+    data=f.read()
 
+#To remove ssl warnings
+if "urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)" in data:
+    pass
+else:
+    warning="import urllib3,requests\nurllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)\n"
+    data=warning+data
+
+#To filter successful requests
+ifLoopAdd="\nif response.status_code==200 or response.status_code==201: \n     print('A request passed:' ,response.status_code)"
+
+if "response.status_code" in data:
+    pass
+else:
+    data=data+ifLoopAdd
 def visit():
-    r=requests.get(url)
+    exec(data)
 
 before=perf_counter()
-with ThreadPoolExecutor(max_workers=threadNo) as executor:
+
+#Change number of request as per requirment
+NumberOfRequests=30
+with cp.ThreadPoolExecutor(max_workers=60) as executor:
     x=0
-    while x<=30:
-        future=executor.submit(visit)
-        x+=1
+    while x <=NumberOfRequests :
+        executor.submit(visit)
+        x += 1
 after=perf_counter()
 
-print(f"Completed in {after-before: 3f} Seconds")
+print(f"\nAttack completed in {after-before: 3f} seconds.\nPlease check into application for better results.")
