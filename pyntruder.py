a80cb8815337d3f9f84fcbc3b723bba31e40ddd5
Vedant Wayal <wayal.vedant@gmail.com>
2022-02-20 20:13:47 +0530

    Update pyntruder.py

--- pyntruder.py
+++ pyntruder.py
@@ -1,7 +1,7 @@
-import concurrent.futures as cp
+from concurrent.futures import ThreadPoolExecutor
 from time import perf_counter
 from sys import argv
-import uncurl
+from uncurl import parse
 
 print("Attack Started, please wait..\n")
 
@@ -9,7 +9,7 @@ with open(argv[1],"r") as f:
     data=f.read()
 data=data.replace("$","")
 data=data.replace("\\","")
-data=uncurl.parse(data)
+data=parse(data)
 data="response="+data
 
 #To remove ssl warnings
@@ -20,7 +20,7 @@ else:
     data=warning+data
 
 #To filter successful requests
-ifLoopAdd="\nif response.status_code==200 or response.status_code==201: \n     print('A request passed:' ,response.status_code)"
+ifLoopAdd="\nif response.status_code==200 or response.status_code==201: \n     print('A request passed:' ,response.status_code)\nelif response.status_code==401 or response.status.code==500:\n      print('Session Expired. Status code',response.status_code)"
 
 if "response.status_code" in data:
     pass
@@ -36,7 +36,7 @@ before=perf_counter()
 #Change number of request as per requirment
 NumberOfRequests=30
 conRequests=60
-with cp.ThreadPoolExecutor(max_workers=conRequests) as executor:
+with ThreadPoolExecutor(max_workers=conRequests) as executor:
     x=0
     while x <=NumberOfRequests :
         executor.submit(visit)
